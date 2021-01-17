1c14de6994479f6afe4343bc3e249a10ee1b112d
Sammy Robens-Paradise <srobensparadise@gmail.com>
2021-01-16 20:24:43 -0500

    added retrieve text data

--- app.js
+++ app.js
@@ -1,64 +1,65 @@
-'use strict';
-const express = require('express');
-var AWS = require('aws-sdk');
-const { v4: uuidv4 } = require('uuid');
-const bodyParser = require('body-parser');
-const { default: axios } = require('axios');
-const env = require('./env');
-require('dotenv').config();
+'use strict'
+const express = require('express')
+var AWS = require('aws-sdk')
+const { v4: uuidv4 } = require('uuid')
+const bodyParser = require('body-parser')
+const { default: axios } = require('axios')
+const env = require('./env')
+const e = require('express')
+require('dotenv').config()
 // express
-const app = express();
-const port = 8000;
+const app = express()
+const port = 8000
 
-app.use(bodyParser.json({ limit: '50mb' })); // support json encoded bodies
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true })); // support encoded bodies
-app.use(express.json());
+app.use(bodyParser.json({ limit: '50mb' })) // support json encoded bodies
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true })) // support encoded bodies
+app.use(express.json())
 
 AWS.config.update({
     region: 'us-east-1',
-});
+})
 
 const secure = (req, res, next) => {
-    const bearerHeader = req.headers['authorization'];
+    const bearerHeader = req.headers['authorization']
     if (bearerHeader) {
-        const bearer = bearerHeader.split(' ');
-        const bearerToken = bearer[1];
+        const bearer = bearerHeader.split(' ')
+        const bearerToken = bearer[1]
         if (bearerToken === process.env.SECURE_KEY) {
-            next();
+            next()
         } else {
-            res.sendStatus(403);
+            res.sendStatus(403)
         }
     } else {
-        res.sendStatus(403);
+        res.sendStatus(403)
     }
-};
+}
 
-var docClient = new AWS.DynamoDB.DocumentClient();
+var docClient = new AWS.DynamoDB.DocumentClient()
 var rekognition = new AWS.Rekognition({
     apiVersion: '2016-06-27',
     accessKeyId: env.AWS_ACCESS_KEY_ID,
     secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
     region: 'us-east-1',
-});
+})
 // http codes
-const HTTP_OK_200 = 200;
-const SUCCESS = 'success';
-const TABLE = 'aws-dynamodb-starter';
+const HTTP_OK_200 = 200
+const SUCCESS = 'success'
+const TABLE = 'aws-dynamodb-starter'
 // Routes
 app.get('/', secure, (req, res) => {
     res.send({
         message: `Request received: ${req.method} - ${req.path}`,
         status: HTTP_OK_200,
         success: SUCCESS,
-    });
-});
+    })
+})
 
 // USER ENDPOINTS
 app.post('/user/add', secure, (req, res) => {
-    let d = new Date();
-    const user_email = req.body.email;
-    const user_id = req.body.sub;
-    const date = d.toISOString();
+    let d = new Date()
+    const user_email = req.body.email
+    const user_id = req.body.sub
+    const date = d.toISOString()
     const params = {
         TableName: TABLE,
         Item: {
@@ -70,97 +71,113 @@ app.post('/user/add', secure, (req, res) => {
                 images: [],
             },
         },
-    };
+    }
     docClient.put(params, (err, data) => {
         if (err) {
-            res.send(err);
+            res.send(err)
         } else {
-            console.log('Added item:', JSON.stringify(data, null, 2));
+            console.log('Added item:', JSON.stringify(data, null, 2))
             res.send({
                 data: data,
                 parameters: params,
                 status: HTTP_OK_200,
                 success: SUCCESS,
-            });
+            })
         }
-    });
-});
+    })
+})
 
 app.get('/user', secure, (req, res) => {
-    const user_email = req.body.email;
-    const user_id = req.body.sub;
+    const user_email = req.body.email
+    const user_id = req.body.sub
     const params = {
         TableName: TABLE,
         Key: {
             ID: user_id,
             email: user_email,
         },
-    };
+    }
     docClient.get(params, (err, data) => {
         if (err) {
-            res.send(err);
+            res.send(err)
         } else {
-            res.send({ data: data, status: HTTP_OK_200, success: SUCCESS });
+            res.send({ data: data, status: HTTP_OK_200, success: SUCCESS })
         }
-    });
-});
+    })
+})
 
 app.get('/user/images', secure, (req, res) => {
-    const user_email = req.body.email;
-    const user_id = req.body.sub;
+    const user_email = req.body.email
+    const user_id = req.body.sub
     const params = {
         TableName: TABLE,
         Key: {
             ID: user_id,
             email: user_email,
         },
-    };
+    }
     docClient.get(params, (err, data) => {
         if (err) {
-            res.send(err);
+            res.send(err)
         } else {
             res.send({
                 data: data.Item.info.images,
                 status: HTTP_OK_200,
                 success: SUCCESS,
-            });
+            })
         }
-    });
-});
+    })
+})
 
 app.get('/user/get-image-url', secure, async (req, res) => {
     try {
         const raw = await axios.get(
             'https://7kdqv9hdsd.execute-api.us-east-1.amazonaws.com/default/getPresignedURL'
-        );
-        res.send(raw.data);
+        )
+        res.send(raw.data)
     } catch (err) {
-        res.send(err);
+        res.send(err)
     }
-});
+})
 
 app.post('/user/retrieve-image-text', secure, async (req, res) => {
-    const { Key, Bucket } = req.body;
-    const params = { Image: { S3Object: { Bucket: Bucket, Name: Key } } };
+    const { Key, Bucket } = req.body
+    const params = { Image: { S3Object: { Bucket: Bucket, Name: Key } } }
     rekognition.detectText(params, (err, data) => {
         if (err) {
-            res.status(500).send(`${err}`);
+            res.status(500).send(`${err}`)
         } else {
-            res.status(200).send(data);
+            res.status(200).send(data)
         }
-    });
-});
+    })
+})
 
-app.post('/user/get-text-data', secure, async (req, res) => {});
+app.post('/user/retrieve-text-data', secure, async (req, res) => {
+    const MAX_QUERY_LENGTH = 1500
+    const query = 'dogs'
+    const URI = encodeURIComponent(query)
+    if (URI.length > MAX_QUERY_LENGTH) {
+        res.status(414).send('Query string too long')
+    }
+    const config = {
+        headers: { 'Ocp-Apim-Subscription-Key': env.AZURE_API_KEY_1 },
+    }
+    const raw = await axios.get(`${env.BING_ENDPOINT}?q=${URI}`, config)
+    if (raw.status !== HTTP_OK_200) {
+        res.status(raw.status).send('Search Encountered an Error')
+    } else {
+        res.status(200).send(raw)
+    }
+})
 
 // Error handler
 app.use((err, req, res) => {
-    console.error(err);
-    res.status(500).send('Internal Serverless Error');
-});
+    console.error(err)
+    res.status(500).send('Internal Serverless Error')
+})
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});
+    console.log(`Example app listening at http://localhost:${port}`)
+})
 
-module.exports = app;
+module.exports = app
